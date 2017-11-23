92bdf2d182fbe9a80a6d771ecbc9e771c4248368
Antarctic-wcg <wcgui@yahoo.com>
2017-11-23 15:39:03 +0800

    1

--- routes/show.js
+++ routes/show.js
@@ -4,13 +4,24 @@ var assert = require('assert');
 var db = require('../db');
 
 
+// router.get('/', function(req, res, next) {
+//   // db.user.find({}, function(err, doc){
+//     // assert.equal(null, err);
+//     // res.json(doc);
+//
+//     res.render('show', { title: '学生信息管理' });
+//   // })
+//
+// });
+
 router.get('/', function(req, res, next) {
-  // db.user.find({}, function(err, doc){
-    // assert.equal(null, err);
-    // res.json(doc);
-    res.render('show', { title: '学生信息管理' });
-  // })
+  db.user.find({}, function(err, doc){
+    assert.equal(null, err);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.json(doc);
+  }).sort({_id:-1})
 
 });
+
 
 module.exports = router;
