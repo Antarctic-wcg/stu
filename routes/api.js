32d46523c109c53bb31b8ac314ed3b98c767ba3e
Antarctic-wcg <wcgui@yahoo.com>
2017-11-23 15:50:56 +0800

    1

--- routes/api.js
+++ routes/api.js
@@ -5,12 +5,12 @@ var db = require('../db');
 
 
 router.get('/', function(req, res, next) {
-  db.user.find({}, function(err, doc){
-    assert.equal(null, err);
-    res.header("Access-Control-Allow-Origin", "*");
-    res.json(doc);
-  }).sort({_id:-1})
-
+  // db.user.find({}, function(err, doc){
+  //   assert.equal(null, err);
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   res.json(doc);
+  // }).sort({_id:-1})
+  res.json({a:1234567})
 });
 
 module.exports = router;
