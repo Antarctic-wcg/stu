602da46e1d6d439423cf8a9e0ecfa993fb6266a9
Antarctic-wcg <wcgui@yahoo.com>
2018-01-09 15:21:22 +0800

    1

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var api_info = require('./routes/api_info');
 
 var app = express();
 
-app.all('/test', function(req, res, next) {
+app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
